2975335f218e8dd6e6e302ab0eb88faa9c071b25
Viraj18P <viraj082006@gmail.com>
2025-06-22 00:36:52 +0530

    Update PC.v

--- PC/PC.v
+++ PC/PC.v
@@ -1,1 +1,22 @@
+module program_counter (
+  input wire clk,
+  input wire ld,
+  input wire reset,
+  input wire pc_enable,
+  input wire [7:0]inp,
+  output reg [7:0]out
+);
 
+  always @(posedge clk)begin
+//doubt check priority of load vs reset
+    if(ld)begin
+      out<=inp;
+    end
+    else if(reset)begin
+      out<=8'h000;
+    end
+    else if(pc_enable)begin
+      out<=out+1;
+    end
+  end
+endmodule
