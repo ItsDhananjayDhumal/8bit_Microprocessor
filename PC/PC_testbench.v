85af018d61846ef65c38b130a7193d49d4b47f33
Viraj18P <viraj082006@gmail.com>
2025-06-22 14:08:52 +0530

    Update PC_testbench.v

--- PC/PC_testbench.v
+++ PC/PC_testbench.v
@@ -3,8 +3,8 @@ module pc_testbench;
   reg ld;
   reg reset;
   reg pc_enable;
-  reg [7:0]inp;
-  wire [7:0]out;
+  reg [31:0]inp;
+  wire [31:0]out;
 
   program_counter dut(
     .clk(clk),
@@ -22,45 +22,45 @@ module pc_testbench;
     ld=0;
     reset=1;
     pc_enable=0;
-    inp=8'h00;
+    inp=32'h00000;
 
-    $display("Time | Reset | PC_En | Load | Input | Output");
-    $display("-----|-------|-------|---------|-------|-------");
+    $display("Time | Reset | PC_En |  Load   |  Input   |  Output  ");
+    $display("-----|-------|-------|---------|----------|----------");
 
     //reset check
     #10 reset=0;
-    $display("%4d |   %b   |   %b   |    %b    |  %02d   |   %02d", 
+    $display("%4d |   %b   |   %b   |    %b    |  %05d   |   %05d", 
                  $time, reset, pc_enable, ld, inp, out);
 
     //normal flow check
     #10 pc_enable=1;
       repeat(5)begin
-        #10 $display("%4d |   %b   |   %b   |    %b    |  %02d   |   %02d",
+        #10 $display("%4d |   %b   |   %b   |    %b    |  %05d   |   %05d",
                      $time,reset,pc_enable,ld,inp,out);
       end
 
     //jump check
-    #10 ld = 1; inp = 8'h18; pc_enable = 0;
-    #10 $display("%4d |   %b   |   %b   |    %b    |  %02d   |   %02d", 
+    #10 ld = 1; inp = 32'h00018; pc_enable = 0;
+    #10 $display("%4d |   %b   |   %b   |    %b    |  %05d   |   %05d", 
                  $time, reset, pc_enable, ld, inp, out); 
 
     //normal flow continue check
     #10 ld = 0; pc_enable = 1;
          repeat(3) begin
-           #10 $display("%4d |   %b   |   %b   |    %b    |  %02d   |   %02d", 
+           #10 $display("%4d |   %b   |   %b   |    %b    |  %05d   |   %05d", 
                        $time, reset, pc_enable, ld, inp, out);
          end
 
     //hold check
     #10 pc_enable = 0;
         repeat(2) begin
-          #10 $display("%4d |   %b   |   %b   |    %b    |  %02d   |   %02d", 
+          #10 $display("%4d |   %b   |   %b   |    %b    |  %05d   |   %05d", 
                       $time, reset, pc_enable, ld, inp, out);
         end
 
     //reset check
     #10 reset = 1;
-    #10 $display("%4d |   %b   |   %b   |    %b    |  %02d   |   %02d", 
+    #10 $display("%4d |   %b   |   %b   |    %b    |  %05d   |   %05d", 
                 $time, reset, pc_enable, ld, inp, out);
 
    #10 $finish;
