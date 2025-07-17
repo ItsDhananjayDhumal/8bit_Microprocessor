40a59b58d5001047c1244c1265dff85c69b3e20b
dhananjay <dhananjaydhumalpresent@gmail.com>
2025-07-18 00:57:19 +0530

    Update forwarding_unit.v

--- src/forwarding_unit.v
+++ src/forwarding_unit.v
@@ -3,7 +3,7 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date: 14.07.2025 23:34:42
+// Create Date: 17.07.2025 13:26:56
 // Design Name: 
 // Module Name: forwarding_unit
 // Project Name: 
@@ -20,35 +20,31 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module forward_unit(
-    input [31:0] EX_rs, EX_rt,
-    input [31:0] MEM_rd, WB_rd,
+module forwarding_unit(
+    input [4:0] EX_rs, EX_rt,
+    input [4:0] MEM_rd, WB_rd,
     input MEM_RegWrite, WB_RegWrite,
     output reg [1:0] ForwardA, ForwardB
 );
 
 always @(*) begin
-    // Default
-    ForwardA = 2'b00;
-    ForwardB = 2'b00;
 
     // EX hazard
     if (MEM_RegWrite && (MEM_rd != 5'd31) && (MEM_rd == EX_rs))
         ForwardA = 2'b10;
-    if (MEM_RegWrite && (MEM_rd != 5'd31) && (MEM_rd == EX_rt))
-        ForwardB = 2'b10;
-
     // MEM hazard (only if no EX hazard)
-    if (WB_RegWrite && (WB_rd != 5'd31) && 
-        !(MEM_RegWrite && (MEM_rd != 5'd31) && (MEM_rd == EX_rs)) &&
-        (WB_rd == EX_rs))
+    else if (WB_RegWrite && (WB_rd != 5'd31) && !(MEM_RegWrite && (MEM_rd != 5'd31) && (MEM_rd == EX_rs)) && (WB_rd == EX_rs))
         ForwardA = 2'b01;
+    else
+        ForwardA = 2'b00;
 
-    if (WB_RegWrite && (WB_rd != 5'd31) && 
-        !(MEM_RegWrite && (MEM_rd != 5'd31) && (MEM_rd == EX_rt)) &&
-        (WB_rd == EX_rt))
-        ForwardB = 2'b01;
+    if (WB_RegWrite && (WB_rd != 5'd31) && !(MEM_RegWrite && (MEM_rd != 5'd31) && (MEM_rd == EX_rt)) && (WB_rd == EX_rt))
+        ForwardB = 2'b01;    
+    else if (MEM_RegWrite && (MEM_rd != 5'd31) && (MEM_rd == EX_rt))
+        ForwardB = 2'b10;    
+    else
+        ForwardB = 2'b00; 
+           
 end
 
 endmodule
-
