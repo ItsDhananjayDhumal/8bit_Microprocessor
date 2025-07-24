36f7fd3e25425621c4c173b54cd432fcb0cea628
dhananjay <dhananjaydhumalpresent@gmail.com>
2025-07-24 19:44:33 +0530

    added flush

--- src/IFID.v
+++ src/IFID.v
@@ -20,17 +20,28 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module IFID(IF_instruction, IF_pcplus4, ID_instruction, ID_pcplus4, clk, IFID_write);
+module IFID(IF_instruction, IF_pcplus4, ID_instruction, ID_pcplus4, clk, IFID_write, reset, IFID_flush);
 
-input clk;
-input wire IFID_write;
+input clk, reset;
+input wire IFID_write, IFID_flush;
 input [31:0] IF_instruction, IF_pcplus4;
 output reg [31:0] ID_instruction, ID_pcplus4;
 
 always @(posedge clk) begin
-    if (IFID_write) begin
-        ID_instruction <= IF_instruction;
-        ID_pcplus4 <= IF_pcplus4;    
+    if (~reset) begin
+        if (IFID_flush == 1'b1) begin
+            ID_instruction <= {{6{1'b1}}, {25{1'b0}}};
+        end
+        else begin
+            if (IFID_write) begin
+                ID_instruction <= IF_instruction;
+                ID_pcplus4 <= IF_pcplus4;    
+            end
+        end
+    end
+    else begin
+        ID_instruction <= 0;
+        ID_pcplus4 <= 0;  
     end
 end    
 
