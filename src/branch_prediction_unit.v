b3562f6dd25028ef2f7b7ab41b525033d5f6445c
dhananjay <dhananjaydhumalpresent@gmail.com>
2025-07-26 12:45:52 +0530

    modified for JSR

--- src/branch_prediction_unit.v
+++ src/branch_prediction_unit.v
@@ -30,22 +30,30 @@ output reg IFID_flush, pcsrc;
 
 wire [31:0] branch_addr;
 wire [5:0] opcode;
-wire [31:0] jump_addr;
+wire [31:0] jump_addr, jr_addr;
 
 parameter JUMP = 6'b000010,
+          JAL = 6'b000110,
+          JR = 6'b000111,
           BEQ = 6'b000100,
           BNE = 6'b000001,
           BLT = 6'b000011,
           BGE = 6'b000101;
 
 assign jump_addr = {ID_pcplus4[31:28], ID_instruction[25:0], 2'b00}; //jump addr
+assign jr_addr = {ID_pcplus4[31:10], ID_read_data1, 2'b00};          // return address
 assign branch_addr = ID_pcplus4 + {{14{ID_instruction[15]}}, ID_instruction[15:0], 2'b00}; // branch addr
 assign opcode = ID_instruction[31:26]; 
 
 always @(*) begin
 
-    if (opcode == JUMP) begin
+    if (opcode == JUMP || opcode == JAL) begin
         pc_addr =  jump_addr;
+        pcsrc = 1'b1;
+        IFID_flush = 1'b1;
+    end
+    else if (opcode == JR) begin
+        pc_addr = jr_addr;
         pcsrc = 1'b1;
         IFID_flush = 1'b1;
     end
