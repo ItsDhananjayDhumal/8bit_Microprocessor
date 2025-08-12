24094314660db880a319c62e70ba1dd543d0d45b
dhananjay <dhananjaydhumalpresent@gmail.com>
2025-08-12 16:53:11 +0530

    Update ram_256B.v

--- src/RAM_256B/ram_256B.v
+++ src/RAM_256B/ram_256B.v
@@ -22,12 +22,14 @@
 
 module ram_256B(
     input       clk,
+    input       reset,
     input       MemWrite,
     input       MemRead,
     input [7:0] addr, //8 bit address
     input [7:0] wdata, // write data to memory
     
-    output reg [7:0] out  // output (at given address)
+    output reg [7:0] out,  // output (at given address)
+    output reg [7:0] fb
     );
     
 reg [7:0]mem [255:0];
@@ -43,12 +45,36 @@ reg [7:0]mem [255:0];
 always @(posedge clk) begin
     if (MemWrite)
         mem[addr] <= wdata;
+        
+    if (reset)
+        mem[0] <= 8'd0;
 end
 
 always @(*) begin
     if (MemRead)
         out = mem[addr];
-end   
- 
+        
+    fb = mem[0];
+end  
+
+
+//ila_1 inst1(.clk(clk),
+//            .probe0(mem[0]),
+//            .probe1(mem[1]),
+//            .probe2(mem[2]),
+//            .probe3(mem[3]),
+//            .probe4(mem[4]),
+//            .probe5(mem[5]),
+//            .probe6(mem[6]),
+//            .probe7(mem[7]),
+//            .probe8(mem[255]),
+//            .probe9(mem[254]),
+//            .probe10(mem[253]),
+//            .probe11(mem[252]),
+//            .probe12(mem[251]),
+//            .probe13(mem[250]),
+//            .probe14(mem[249]),
+//            .probe15(mem[248]));
+
 endmodule
 
