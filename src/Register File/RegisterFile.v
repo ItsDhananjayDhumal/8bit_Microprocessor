1dad4cd679654f9b4d1f1757cacdca859a780935
dhananjay <dhananjaydhumalpresent@gmail.com>
2025-08-12 16:52:39 +0530

    Update RegisterFile.v

--- src/Register File/RegisterFile.v
+++ src/Register File/RegisterFile.v
@@ -20,8 +20,10 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module RegisterFile(
+(*dont_touch = "true"*) module RegisterFile(
+    input wire reset,
     input wire clk,
+    input wire btn0, btn1, btn2, btn3,
     input wire RegWrite,
     input wire [4:0] rs,      // Read register 1
     input wire [4:0] rt,      // Read register 2
@@ -40,6 +42,20 @@ module RegisterFile(
     always @(posedge clk) begin
         if (RegWrite)
             registers[rd] <= write_data;
+        if (reset)
+            registers[29] <= 8'b11111111; // stack pointer
+            
+        registers[0] <= {4'd0, btn0, btn1, btn2, btn3};
     end
+    
+//    ila_0 inst (.clk(clk),
+//                .probe0(registers[0]),
+//                .probe1(registers[1]),
+//                .probe2(registers[2]),
+//                .probe3(registers[3]),
+//                .probe4(registers[4]),
+//                .probe5(registers[5]),
+//                .probe6(registers[6]),
+//                .probe7(registers[7]));                                
 endmodule
 
